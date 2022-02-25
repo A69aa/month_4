6917dc92db4ef7fe2df521c60219beb168c61539
A69aa <96340875+A69aa@users.noreply.github.com>
2022-02-25 18:07:43 +0300

    Add files via upload

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -5,7 +5,8 @@ from movie_app.models import Director, Review,Movie
 class DirectorSerializers(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = 'name count_movies'.split()
+        fields = '__all__'
+        # fields = 'name count_movies'.split()
 
 class ReviewSerializers(serializers.ModelSerializer):
     class Meta:
@@ -15,5 +16,5 @@ class ReviewSerializers(serializers.ModelSerializer):
 class MovieSerializers(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = 'title description duration director'.split()
+        fields = 'id title description duration director'.split()
 
